f6cee6030fbcba31a59ab4035218d0de95ecf423
Nikita <bigmama93@mail.ru>
2022-10-23 23:44:27 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,67 @@
+# Сервис получения погоды 
+Cервис интегрируется с API [сайта](https://openweathermap.org/)
+
+## Описание задания
+
+- Реализовать API, которое на HTTP-запрос GET /weather/<city_name>, 
+  где <city_name> - это название города на английском языке, 
+  возвращает текущую температуру в этом городе в градусах Цельсия, 
+  атомсферное давление (мм рт.ст.) и скорость ветра (м/c).
+
+### Как запустить dev-версию 
+Скачайте код:
+```sh
+git clone https://github.com/ZusmanOne/weather_api.git
+```
+
+Перейдите в каталог проекта:
+```sh
+cd weather_api
+```
+[Установите Python](https://www.python.org/), если этого ещё не сделали.
+
+Проверьте, что `python` установлен и корректно настроен. Запустите его в командной строке:
+```sh
+python --version
+```
+
+В каталоге проекта создайте виртуальное окружение:
+```sh
+python -m venv venv
+```
+Активируйте его. На разных операционных системах это делается разными командами:
+
+- Windows: `.\venv\Scripts\activate`
+- MacOS/Linux: `source venv/bin/activate`
+
+Установите зависимости в виртуальное окружение:
+```sh
+pip install -r requirements.txt
+```
+Настройка: создать файл `.env` в каталоге `payment_project/` со следующими настройками:
+
+`CACHE_TYPE`=redis
+`CACHE_REDIS_HOST`=redis
+`CACHE_REDIS_PORT`=6379
+`CACHE_REDIS_DB`=0
+`CACHE_REDIS_URL`=redis://localhost:6379/0
+`CACHE_DEFAULT_TIMEOUT`=500
+`FLASK_DEBUG`=True
+`API_KEY`- получите api ключ при регистрации на [сайте](https://openweathermap.org/)
+
+Настройка: создать файл `.flaskenv` в каталоге `payment_project/` со следующими настройками:
+`FLASK_APP`=app
+
+Запустить сервер
+```sh
+flask run
+```
+
+Сервис имеют следующую маршрутизацию
+
+
+
+```http://127.0.0.1:8000/weather/<city>/``` - получит координаты города и перенаправит на результат погоды
+
+
+
--- core/routes.py
+++ core/routes.py
@@ -4,9 +4,11 @@ from core import app
 from core import cache
 
 
+API_KEY = app.config.get("API_KEY")
+
 @app.route('/weather/<city>')
 def get_city_coordinates(city):
-    url_city = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={app.config.get("API_KEY")}'
+    url_city = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}'
     response = requests.get(url_city)
     city_serializer = response.json()
     lat = city_serializer[0]['lat']
@@ -17,18 +19,17 @@ def get_city_coordinates(city):
 @app.route('/', methods=['GET'])
 @cache.cached(timeout=1800, query_string=True)
 def get_weather():
-    url = 'https://api.openweathermap.org/data/2.5/weather'
-    search1 = request.args.get('lat')
-    search2 = request.args.get('lon')
-    search3 = app.config.get("API_KEY")
-    r = requests.get(url, params={'lat': search1,
-                                  'lon': search2,
-                                  'appid': search3,
+    weather_url = 'https://api.openweathermap.org/data/2.5/weather'
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    response = requests.get(weather_url, params={'lat': lat,
+                                  'lon': lon,
+                                  'appid': API_KEY,
                                   'units': 'metric'})
-    my_json = r.json()
-    location_temp = my_json['main']['temp']
-    location_pressure = my_json['main']['pressure'] // 1.333
-    wind_speed = my_json['wind']['speed']
+    json_string = response.json()
+    location_temp = json_string['main']['temp']
+    location_pressure = json_string['main']['pressure'] // 1.333
+    wind_speed = json_string['wind']['speed']
     weather_data = {'air temperature': f'{location_temp} c',
                     'atmospheric pressure': f'{location_pressure} mm Hg',
                     'wind speed': f'{wind_speed} m/s'}
